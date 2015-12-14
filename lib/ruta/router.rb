bbe31efae1561a207fa9fa977e741a64c7ed9882
Martin Becker <mbeckerwork@gmail.com>
2015-12-14 17:24:28 +0000

    add router documentation

--- lib/ruta/router.rb
+++ lib/ruta/router.rb
@@ -9,38 +9,46 @@ module Ruta
     # @!attribute [r,w] current_context
     # @return [Array<Symbol>] current_context a list of contexts, the last being the current
 
-    # @!attribute [r,w] routes
-    # @return [{ref => Proc}] list of route handlers attached to this context
-
     attr_accessor :current_context
+
+    # @see (see #Router#define)
     def initialize block
       @current_context = []
       Context.define(:no_context)
       instance_exec &block
     end
 
+    # set which Context to map the following routes to
+    #
+    # @param [Symbol] context to map routes to
     def for_context context
       @current_context << context
       yield
       @current_context.pop
     end
 
-    def get_context
-      @current_context.last || :no_context
-    end
-
+    #  map a route
+    # @param [Symbol] ref to map route to for easy future reference
     def map ref,route, options={}
       context = Context.collection[get_context]
       context.routes[ref]= Route.new(route, context,options)
     end
 
-    def root_to ref
-      Router.set_root_to ref
+    # set the root context, this is the initial context that will be renered by the router
+    #
+    # @note there is only ever one root, calling this multiple times will over right the original root
+    # @param [Symbol] reference to context
+    def root_to reference
+      Router.set_root_to reference
       context = Context.collection[:no_context]
-      context.routes[:root]= Route.new('/', context,{ context: ref})
+      context.routes[:root]= Route.new('/', context,{ context: reference})
     end
 
+    private
 
+    def get_context
+      @current_context.last || :no_context
+    end
 
     class << self
 
@@ -58,13 +66,24 @@ module Ruta
 
       attr_reader :current_context, :root
 
+      # define a router, this can be called multiple times
+      # @example defining routes
+      #   Ruta::Router.define do
+      #     for_context :main do
+      #       for_context :info_view do
+      #         map :i_switch, '/buttons/:switch_to', to: [:scroller,:buttons]
+      #         map :sign_up, '/sign_up', context: :sign_up
+      #       end
+      #     end
+      #
+      #     root_to :main
+      #   end
+      # @note please be aware that placing contexts within other contexts doesn't actully do anything.
+      #   however it is planed to be able to mount sub contexts to a route and re-direct as neccasary
+      #   think mounting engines or sub apps in RoR or Padrino
+      # @yield Use this block to define any routes
       def define &block
         new block
-      end
-
-      def set_root_to context
-        @root = context
-        Router.set_context_to root
       end
 
       def set_context_to context
@@ -73,7 +92,6 @@ module Ruta
 
       def find_and_execute(path)
         res = find(path)
-        puts "result:#{res}"
         if res
           navigate_to res
         else
@@ -92,12 +110,16 @@ module Ruta
       end
 
       def navigate_to(route)
-        puts route
         route[:route].execute_handler route[:params],route[:path]
       end
 
       def route_for context, ref,params=nil
         Context.collection[context].routes[ref].get(params)
+      end
+
+      def set_root_to context
+        @root = context
+        Router.set_context_to root
       end
 
     end
